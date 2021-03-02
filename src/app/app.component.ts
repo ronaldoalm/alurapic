f16b91509b865cc063029a9262960387a87233f4
Ronaldo Almeida <ronaldoalm@unifor.br>
2021-03-02 10:52:56 -0300

    Conectando com a API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { PhotoService } from './photos/photo/photo.service';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,15 +10,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
-  photos= [
-    {
-      url: "https://s1.1zoom.me/big0/158/Lamborghini_White_background_Yellow_Metallic_566962_1280x853.jpg",
-      description: "carro amarelo"
-    },
-    {
-      url: "https://image.freepik.com/fotos-gratis/teste-de-carro-sedan-vermelho-na-estrada_114579-4061.jpg",
-      description: "carro vermelho"
-    }
-  ]
+export class AppComponent implements OnInit{
+
+  photos: any[] = [];
+  
+  constructor(private photoService: PhotoService){ }
+
+  ngOnInit() :void {
+    this.photoService.listFromUser('flavio')
+    .subscribe(photos =>  this.photos = photos  );
+  }
+
 }
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from "@angular/core";
 
 import { PhotoComponent } from './photo/photo.component';
@@ -5,6 +6,7 @@ import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
   declarations: [PhotoComponent],
-  exports: [PhotoComponent]
+  exports: [PhotoComponent],
+  imports: [HttpClientModule]
 })
 export class PhotosModule {}
